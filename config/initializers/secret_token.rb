3ff22ac9eb89f10cca56bc45c7c906374a70c819
truongtr <truongtr@me.com>
2015-01-31 05:49:09 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Erierb::Application.config.secret_key_base = '5d6447687f5926f06582056a1a8f1e24f5c5e4825c0f2095c7778a67fcc7c94ab5642393e1815188948f3c0b8b5fdc25ff941caa2d9781b3277b3e5ac00b79b0'
